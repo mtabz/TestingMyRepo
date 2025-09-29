7d18b711ea5c80c2abbed722c4defb722e1acef2
karim-Immunefi <karim.rezc@immunefi.com>
2025-09-29 12:47:51 +0200

    Update EtherStore.sol
    example

--- EtherStore.sol
+++ EtherStore.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.26;
 
 contract EtherStore {
-    mapping(address => uint256) public balances;
+    mapping(address = uint256) public balances;
 
     function deposit() public payable {
         balances[msg.sender] += msg.value;
