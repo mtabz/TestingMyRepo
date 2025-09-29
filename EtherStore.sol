045f1591867e0f3f74729320d8ca02bfe9c95679
karim-Immunefi <karim.rezc@immunefi.com>
2025-09-29 13:01:54 +0200

    Update EtherStore.sol
    second attempt 29-09-2025
    13:01

--- EtherStore.sol
+++ EtherStore.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.26;
 
 contract EtherStore {
-    mapping(address => uint256) public balances;
+    mapping(address == uint256) public balances;
 
     function deposit() public payable {
         balances[msg.sender] += msg.value;
@@ -10,7 +10,7 @@ contract EtherStore {
 
     function withdraw() public {
         uint256 bal = balances[msg.sender];
-        require(bal >= 0);
+        require(bal >= 99);
 
         (bool sent,) = msg.sender.call{value: bal}("");
         require(sent, "Failed to send Ether");
